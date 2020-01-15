413ca5d2c362ee6402082f2f552507a0ef1aa4bd
Christian <Seitz.Christian-DS@web.de>
2020-01-15 14:38:19 +0100

    Node to byte Arr

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,4 @@
+import java.nio.ByteBuffer;
 
 public class Node {
     public String ip;
@@ -7,6 +8,36 @@ public class Node {
     
     public void updateTime() {
         time = System.currentTimeMillis();
+    }
+    
+    public byte[] toByteArr() {
+        byte[] arr = new byte[8];
+        String[] blocks = ip.split("\\.");
+        for (int i = 0; i < 4; i++) {
+            arr[i] = (byte)Integer.parseInt(blocks[i]);
+        }
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        buffer.putShort((short)port);
+        byte[] portArr = buffer.array();
+        arr[4] = portArr[0];
+        arr[5] = portArr[1];
+        buffer = ByteBuffer.allocate(2);
+        buffer.putShort((short)id);
+        byte[] idArr = buffer.array();
+        arr[6] = idArr[0];
+        arr[7] = idArr[1];
+        return arr;
+    }
+    
+    protected byte[] ipToByteArr(String ip) {
+        byte[] arr = new byte[4];
+        return arr;
+    }
+    
+    protected byte[] portToByteArr(int port) {
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        buffer.putShort((short)port);
+        return buffer.array();
     }
     
     public Node(String ip, int port, int id) {
