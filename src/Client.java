bc588ca91554c6d4fdf9afaf042615851b08c37a
Christian <Seitz.Christian-DS@web.de>
2020-01-15 15:09:43 +0100

    Handle incoming connections in Client

--- src/Client.java
+++ src/Client.java
@@ -1,19 +1,34 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Client implements Runnable {
     private int port;
     private String serverIp;
     private int serverPort;
+    private NodeList nodes;
     
     public Client (int port, String serverIp, int serverPort) {
         this.port = port;
         this.serverIp = serverIp;
         this.serverPort = serverPort;
+        nodes = new NodeList(4);
     }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Client: Serversocket gestartet, warte auf eingehende Verbindungen");
+            while (true) {
+                Socket connectionsocket = serverSocket.accept();
+                ConnectionHandler handler = new ConnectionHandler(connectionsocket, nodes);
+                new Thread(handler).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
