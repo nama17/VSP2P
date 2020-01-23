09c66a4d28b39e90559f0453fc3f3ecf32ee23ef
DictumFactum <labor_omnia_vincit@protonmai.com>
2020-01-24 00:20:36 +0100

    all message classes changes

--- src/message/EntryMsg.java
+++ src/message/EntryMsg.java
@@ -5,27 +5,34 @@ import java.net.*;
 import java.util.*;
 import core.*;
 import util.*;
+import java.nio.*;
 
 public class EntryMsg extends Message {
-    public Node node;
+    public String ip;
+    public int port;
 
-    public EntryMsg(Node node){
-        this.node = node;
+    public EntryMsg(String ip, int port){
+        this.ip = ip;
+        this.port = port;
     }
     public void read(InputStream in){
         try{
             in.read();
-            node = new Node(readIp(in), readPort(in), readId(in));
+            ip = readIp(in);
+            port =  readPort(in);
         }catch(IOException e){
             e.printStackTrace();
         } 
     }
     public byte[] create(){
-        if(node != null){
+        if(ip != null){
             byte[] data = new byte[2];
             data[0] = 1;
             data[1] = 0;
-            return ArrayHelper.merge(data,node.toByteArr());
+            ByteBuffer buffer = ByteBuffer.allocate(2);
+            buffer.putShort((short)port);
+            byte[] portArr = buffer.array();
+            return ArrayHelper.merge(ip.getBytes(), portArr);
         }
         else{
             return null;
