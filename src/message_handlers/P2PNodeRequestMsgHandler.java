300a5f7b80c28060a7395e943c8db13eafb7ef5e
DictumFactum <labor_omnia_vincit@protonmai.com>
2020-01-23 03:41:18 +0100

    handlers changed

--- src/message_handlers/P2PNodeRequestMsgHandler.java
+++ src/message_handlers/P2PNodeRequestMsgHandler.java
@@ -1,7 +1,8 @@
 package message_handlers;
+import java.io.*;
 import java.net.*;
-import core.Node;
-import core.NodeList;
+import core.*;
+import java.util.*;
 
 public class P2PNodeRequestMsgHandler extends MsgHandler{
 	private NodeList nodeList;
@@ -15,6 +16,48 @@ public class P2PNodeRequestMsgHandler extends MsgHandler{
 	}
 	@Override
 	public void handle() {
+		try{
+			InputStream in = connectionSocket.getInputStream();
+			in.read(); // Version
+			p2pNodeRequestMsg(in);
+			sendP2pNodeResponseMsg();
+		}catch (IOException e){
+			System.out.println(e);
+		}
 		
+	}
+
+	private void sendP2pNodeResponseMsg() throws IOException {
+        byte[] data = new byte[34];
+        data[0] = 4; // Tag
+        data[1] = 1; // Version
+        nodesToByteArr(nodeList.nodes, data, 2, 4);
+        OutputStream out = connectionSocket.getOutputStream();
+        out.write(data);
+        System.out.println("Client: Node daten auf Anfrage gesendet");
+    }
+	
+	
+	private void p2pNodeRequestMsg(InputStream in) throws IOException {
+		String ip = readIp(in);
+		int port = readPort(in);
+		int id = readId(in);
+		if (ip == null || port == 0) {
+			return;
+		}
+		if (ip.length() == 0 || port == -1) {
+			System.out.println("Client: Ungueltige IP oder Port erhalten");
+			return;
+		}
+		Node node = nodeList.getNode(id);
+		// String ownIp = InetAddress.getLocalHost().getHostAddress();
+		if (node == null && !ip.equals(self.ip) && port != self.port) {
+			synchronized (nodeList) {
+				nodeList.addNode(ip, port, id);
+			}
+			System.out.println("Client: Neue Node hinzugefuegt:");
+			node = new Node(ip, port, id);
+			node.print();
+		}
 	}
 }
--- src/message_handlers/P2PNodeResponseMsgHandler.java
+++ src/message_handlers/P2PNodeResponseMsgHandler.java
@@ -1,8 +1,9 @@
 package message_handlers;
 
 import java.net.*;
-import core.Node;
-import core.NodeList;
+import core.*;
+import java.io.*;
+import java.util.*;
 
 public class P2PNodeResponseMsgHandler extends MsgHandler{
 	private NodeList nodeList;
@@ -16,6 +17,36 @@ public class P2PNodeResponseMsgHandler extends MsgHandler{
 	}
 	@Override
 	public void handle() {
-		
+		try{
+		InputStream in = connectionSocket.getInputStream();
+		in.read(); // Version
+		for (int i = 0; i < 4; i++) {
+			p2pNodeRequestMsg(in);
+		}}catch(IOException e){
+			System.out.println(e);
+		}
+	}
+	
+	private void p2pNodeRequestMsg(InputStream in) throws IOException {
+		String ip = readIp(in);
+		int port = readPort(in);
+		int id = readId(in);
+		if (ip == null || port == 0) {
+			return;
+		}
+		if (ip.length() == 0 || port == -1) {
+			System.out.println("Client: Ungueltige IP oder Port erhalten");
+			return;
+		}
+		Node node = nodeList.getNode(id);
+		// String ownIp = InetAddress.getLocalHost().getHostAddress();
+		if (node == null && !ip.equals(self.ip) && port != self.port) {
+			synchronized (nodeList) {
+				nodeList.addNode(ip, port, id);
+			}
+			System.out.println("Client: Neue Node hinzugefuegt:");
+			node = new Node(ip, port, id);
+			node.print();
+		}
 	}
 }
