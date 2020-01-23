83f5279ef371bae3f395d618b088b6e0146310b3
DictumFactum <labor_omnia_vincit@protonmai.com>
2020-01-23 23:04:40 +0100

    nodesearchmsg

---
+++ src/message/P2PNodeSearchMsg.java
@@ -0,0 +1,49 @@
+package message;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.io.*;
+import core.*;
+import util.*;
+
+public class P2PNodeSearchMsg extends Message {
+    public Node node;
+    public int sourceId;
+    public int searchId;
+    public int destinationId;
+
+    public P2PNodeSearchMsg(Node node, int sourceId, int searchId, int destinationId) {
+        this.node = node;
+        this.sourceId = sourceId;
+        this.searchId = searchId;
+        this.destinationId = destinationId;
+    }
+
+    public void read(InputStream in) {
+        try {
+            node = new Node(readIp(in), readPort(in), readId(in));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] create() {
+        // nodelist should always be of size 4
+        if (node != null) {
+            byte[] data = new byte[2];
+            data[0] = 6;
+            data[1] = 1;
+            byte[] searchData = new byte[6];
+            ByteBuffer buffer = ByteBuffer.wrap(searchData);
+            buffer.putInt(sourceId);
+            buffer.putInt(searchId);
+            buffer.putInt(destinationId);
+            return ArrayHelper.merge(ArrayHelper.merge(data, node.toByteArr()), searchData);
+        } else {
+            return null;
+        }
+    }
+}
