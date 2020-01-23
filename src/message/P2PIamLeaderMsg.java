168a817834f1452a1f29f8f1ce9ddc793abb16e7
DictumFactum <labor_omnia_vincit@protonmai.com>
2020-01-24 00:03:49 +0100

    all message classes

---
+++ src/message/P2PIamLeaderMsg.java
@@ -0,0 +1,39 @@
+package message;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.io.*;
+import core.*;
+import util.*;
+
+public class P2PIamLeaderMsg extends Message {
+    public Node node;
+
+    public P2PIamLeaderMsg(Node node) {
+        this.node = node;
+    }
+
+    public void read(InputStream in) {
+        try {
+            in.read();
+            node = new Node(readIp(in), readPort(in), readId(in));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] create() {
+        // nodelist should always be of size 4
+        if (node != null) {
+            byte[] data = new byte[2];
+            data[0] = 10;
+            data[1] = 1;
+            return ArrayHelper.merge(data, node.toByteArr());
+        } else {
+            return null;
+        }
+    }
+}
