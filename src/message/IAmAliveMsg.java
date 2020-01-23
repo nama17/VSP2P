4e8f5c3c7204a09a2680fb5b2f5ed4cee48cd37d
DictumFactum <labor_omnia_vincit@protonmai.com>
2020-01-23 22:43:47 +0100

    more message classes

---
+++ src/message/IAmAliveMsg.java
@@ -0,0 +1,36 @@
+package message;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.util.*;
+import core.*;
+import util.*;
+
+public class IAmAliveMsg extends Message {
+    public Node node;
+
+    public IAmAliveMsg(Node node) {
+        this.node = node;
+    }
+
+    public void read(InputStream in) {
+        try {
+            node = new Node(readIp(in), readPort(in), readId(in));
+         }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] create() {
+        // nodelist should always be of size 4
+        if (node != null) {
+            byte[] data = new byte[2];
+            data[0] = 5;
+            data[1] = 1;
+            return ArrayHelper.merge(data, node.toByteArr());
+        } else {
+            return null;
+        }
+    }
+}
