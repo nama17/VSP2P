65aab42411ce04967b9b3a0c6531f912c163a30f
DictumFactum <labor_omnia_vincit@protonmai.com>
2020-01-23 22:35:19 +0100

    more messageclasses

---
+++ src/message/P2PNodeRequestMsg.java
@@ -0,0 +1,35 @@
+package message;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.util.*;
+import core.*;
+import util.*;
+
+public class P2PNodeRequestMsg extends Message {
+    public Node node;
+
+    public P2PNodeRequestMsg(Node node) {
+        this.node = node;
+    }
+
+    public void read(InputStream in) {
+        try {
+            this.node = new Node(readIp(in), readPort(in), readId(in));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] create() {
+        if (node != null) {
+            byte[] data = new byte[2];
+            data[0] = 3;
+            data[1] = 1;
+            return ArrayHelper.merge(data, node.toByteArr());
+        } else {
+            return null;
+        }
+    }
+}
--- src/message/P2PNodeResponseMsg.java
+++ src/message/P2PNodeResponseMsg.java
@@ -7,31 +7,32 @@ import java.util.*;
 import core.*;
 import util.*;
 
-public class EntryResponseMsg extends Message {
+public class P2PNodeResponseMsg extends Message {
     public NodeList nodeList;
+    public Node node;
 
-    public EntryResponseMsg(NodeList nodeList) {
+    public P2PNodeResponseMsg(NodeList nodeList) {
         this.nodeList = nodeList;
     }
 
     public void read(InputStream in) {
         try {
-            for(int i=0; i < 4; i++){
+            for(int i = 0; i<4; i++){
                 nodeList.addNode(readIp(in), readPort(in), readId(in));
-            };
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public byte[] create() {
-        //nodelist should always be of size 4
+        // nodelist should always be of size 4
         if (nodeList != null) {
             byte[] data = new byte[2];
-            data[0] = 2;
+            data[0] = 4;
             data[1] = 1;
-            return ArrayHelper.merge(data, ArrayHelper.merge(ArrayHelper.merge(nodeList.nodes.get(1).toByteArr(), nodeList.nodes.get(2).toByteArr()),
-					ArrayHelper.merge(nodeList.nodes.get(3).toByteArr(), nodeList.nodes.get(4).toByteArr())));
+            return ArrayHelper.merge(data,ArrayHelper.merge(ArrayHelper.merge(nodeList.nodes.get(1).toByteArr(), nodeList.nodes.get(2).toByteArr()),
+            ArrayHelper.merge(nodeList.nodes.get(3).toByteArr(), nodeList.nodes.get(4).toByteArr())));
         } else {
             return null;
         }
