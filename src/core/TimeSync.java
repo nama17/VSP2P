be21421e5346f4e7cfc58992a131ff83c1172ddd
Christian Seitz <seitz.christian-ds@web.de>
2020-02-07 10:20:07 +0100

    x

--- src/core/TimeSync.java
+++ src/core/TimeSync.java
@@ -23,42 +23,43 @@ public class TimeSync {
         ArrayList<Long> times = new ArrayList<Long>();
         ArrayList<Thread> threads = new ArrayList<Thread>();
         ArrayList<Socket> sockets = new ArrayList<Socket>();
-        synchronized(nodes) {
-            for (Node node : nodes.nodes) {
-                Thread t = new Thread(() -> {
-                    Socket socket;
-                    try {
-                        socket = new Socket(node.ip, node.port);
-                        OutputStream out = socket.getOutputStream();
-                        InputStream in = socket.getInputStream();
-                        Message timeMsg = new P2PTellMeYourTimeMsg(self);
-                        out.write(timeMsg.create());
-                        System.out.println("Client: P2PTellMeYourTimeMsg gesendet");
-                        boolean res = StreamHelper.waitForTag(in, 200, 12, (int tag) -> {
-                            ConnectionHandler handler = new ConnectionHandler(socket, nodes, self, tag);
-                            Thread thread = new Thread(handler);
-                            thread.start();
-                            try {
-                                thread.join();
-                            } catch (InterruptedException e) {}
-                        });
-                        if (!res) {
-                            return;
-                        }
-                        P2PHereIsMyTimeMsg myTimeMsg = new P2PHereIsMyTimeMsg();
-                        myTimeMsg.read(in);
-                        times.add(myTimeMsg.time);
-                        sockets.add(socket);
-                        Date date = new Date();
-                        date.setTime(myTimeMsg.time);
-                        System.out.println("Client: Zeit von peer empfangen: "  + date);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+        for (int id = self.id - 1 ; id > 0; id--) {
+        	Socket socket = findSocket(id);
+        	if (socket != null) {
+        		sockets.add(socket);
+        	}
+        }
+        for (Socket s : sockets) {
+            Thread t = new Thread(() -> {
+                try {
+                    OutputStream out = s.getOutputStream();
+                    InputStream in = s.getInputStream();
+                    Message timeMsg = new P2PTellMeYourTimeMsg(self);
+                    out.write(timeMsg.create());
+                    System.out.println("Client: P2PTellMeYourTimeMsg gesendet");
+                    boolean res = StreamHelper.waitForTag(in, 200, 12, (int tag) -> {
+                        ConnectionHandler handler = new ConnectionHandler(s, nodes, self, tag);
+                        Thread thread = new Thread(handler);
+                        thread.start();
+                        try {
+                            thread.join();
+                        } catch (InterruptedException e) {}
+                    });
+                    if (!res) {
+                        return;
                     }
-                });
-                threads.add(t);
-                t.start();
-            }
+                    P2PHereIsMyTimeMsg myTimeMsg = new P2PHereIsMyTimeMsg();
+                    myTimeMsg.read(in);
+                    times.add(myTimeMsg.time);
+                    Date date = new Date();
+                    date.setTime(myTimeMsg.time);
+                    System.out.println("Client: Zeit von peer empfangen: "  + date);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            threads.add(t);
+            t.start();
         }
         ThreadHelper.multiJoin(threads);
         long sum = 0;
@@ -83,6 +84,30 @@ public class TimeSync {
         Date date = new Date();
         date.setTime(avg);
         System.out.println("Client: Neue Zeit an alle gesendet: "  + date);
+    }
+    
+    public Socket findSocket(int id) {
+		try {
+	        Node receiver = nodes.getNode(id);
+	        if (receiver == null) {
+	            NodeSearch search = new NodeSearch(nodes, self, (short) id);
+	            Thread t = new Thread(search);
+	            t.start();
+	            try {
+	                t.join();
+	            } catch (InterruptedException e) {}
+	        }
+	        receiver = nodes.getNode(id);
+	        if (receiver == null) {
+	            return null;
+	        }
+	        Socket socket;
+			socket = new Socket(receiver.ip, receiver.port);
+			return socket;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
     }
     
     public TimeSync(NodeList nodes, Node self) {
